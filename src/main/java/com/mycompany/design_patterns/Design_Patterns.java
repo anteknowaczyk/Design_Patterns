08d99a3f55159984ffe299181cc1b9d2bf07aae8
Antoni Nowaczyk <antoninowaczyk1@gmail.com>
2024-11-07 22:28:49 +0100

    second

---
+++ src/main/java/com/mycompany/design_patterns/Design_Patterns.java
@@ -0,0 +1,11 @@
+package com.mycompany.design_patterns;
+
+/**
+ * This project contains the Java implementation of commonly used programming design patterns.
+ * @author Antoni Jan Nowaczyk
+ */
+public class Design_Patterns {
+    public static void main(String[] args) {
+        System.out.println("Let's design!");
+    }
+}
---
+++ src/main/java/com/mycompany/design_patterns/Structural/Composite.java
@@ -0,0 +1,9 @@
+package com.mycompany.design_patterns.Structural;
+
+/**
+ * A Java implementation of Composite design pattern.
+ * @author Antoni Jan Nowaczyk
+ */
+public class Composite {
+    
+}
